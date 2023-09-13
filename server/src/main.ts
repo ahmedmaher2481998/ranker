66b132c104599e551ba3292c4b07d7c281ba23e4
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2023-09-13 10:32:23 +0300

    added polls repoistory and tested it

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,11 +4,11 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const logger = new Logger('Main (main.ts)');
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
-  const clientPort = configService.get('PORT_CLIENT');
+  const clientPort = parseInt(configService.get('PORT_CLIENT'));
   const port = parseInt(configService.get('PORT'));
 
   app.enableCors({
@@ -17,6 +17,7 @@ async function bootstrap() {
       /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/,
     ],
   });
+
   await app.listen(port);
 
   logger.log(`Server running on port ${port}`);
