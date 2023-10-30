ec8ca0f0475cbaf7de2038b520a5a3b00bdf536c
Ahmed maher <ahmedmaher2481998@yahoo.com>
2023-10-30 15:08:24 +0200

    done with the main pages setup

--- client/src/Pages.tsx
+++ client/src/Pages.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSnapshot } from 'valtio';
 import Welcome from './pages/Welcome';
-import { AppPage } from './state';
+import { AppPage, state } from './state';
 import Create from './pages/Create';
 import Join from './pages/Join';
+import { CSSTransition } from 'react-transition-group';
 
 const routeConfig = {
   [AppPage.welcome]: Welcome,
@@ -10,10 +12,23 @@ const routeConfig = {
   [AppPage.join]: Join,
 };
 const Pages = () => {
+  const currentState = useSnapshot(state);
   return (
-    <div className="page mobile-height max-w-screen-sm mx-auto py-8 px-4 overflow-y-auto">
-      <Welcome />
-    </div>
+    <>
+      {Object.entries(routeConfig).map(([page, Component]) => (
+        <CSSTransition
+          key={page}
+          in={page === currentState.currentPage}
+          timeout={300}
+          classNames={'page'}
+          unmountOnExit
+        >
+          <div className="page mobile-height max-w-screen-sm mx-auto py-8 px-4 overflow-y-auto">
+            <Component />
+          </div>
+        </CSSTransition>
+      ))}
+    </>
   );
 };
 
--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Join = () => {
+const Join: React.FC = () => {
   return (
     <div className="flex flex-col w-full justify-around items-stretch  h-full mx-auto max-w-sm ">
       <h3 className="text-center">Enter Code Provided by &qout;Friend&qout;</h3>
